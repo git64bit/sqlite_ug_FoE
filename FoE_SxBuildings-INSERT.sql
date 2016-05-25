71dc274ed5f6500fb1b4b82cab4dc80a83e1a71c
Arc-Hives <sandork@arc-tronics.com>
2016-05-25 12:05:19 -0500

    Created FoE_SxBuildings-INSERT.sql
    Just as the Cx buildings the records are added following the Tech-tree. No Special or Great Buildings are considered for this table yet.
    
    After running the script sqlite responds with "Rows affected: 12"

---
+++ FoE_SxBuildings-INSERT.sql
@@ -0,0 +1,144 @@
+-- Add building - Pottery
+INSERT INTO FoE_SxBuildings (
+  SxID
+, SxName
+, SxAge
+, SxTech
+, SxPopRequired
+, SxSupplyYieldFiveMinutes_AA
+, SxSupplyYieldFifteenMinutes_BB
+, SxSupplyYieldOneHour_CC
+, SxSupplyYieldFourHours_DD
+, SxSupplyYieldEightHours_EE
+, SxSupplyYieldOneDay_FF
+, SxSupplyYieldTwoDays_GG
+, SxSWPixels
+, SxNWPixels
+, SxRoadWidth
+, SxBuildTime
+, SxBuildDiamonds
+, SxBuildCoins
+, SxBuildSupplies
+, SxSellCoins
+, SxSellSupplies
+, dbStatusID
+) VALUES (
+  150150 -- SxID
+, 'Pottery' -- SxName
+, 'Bronze Age' -- SxAge
+, 'Pottery' -- SxTech
+, 41 -- SxPopRequired
+, 8 -- SxSupplyYieldFiveMinutes_AA
+, 21 -- SxSupplyYieldFifteenMinutes_BB
+, 50 -- SxSupplyYieldOneHour_CC
+, 83 -- SxSupplyYieldFourHours_DD
+, 125 -- SxSupplyYieldEightHours_EE
+, 249 -- SxSupplyYieldOneDay_FF
+, -1 -- SxSupplyYieldTwoDays_GG
+, 4 -- SxSWPixels
+, 3 -- SxNWPixels
+, 1 -- SxRoadWidth
+, 20 -- SxBuildTime
+, 0 -- SxBuildDiamonds
+, 237 -- SxBuildCoins
+, 29 -- SxBuildSupplies
+, -1 -- SxSellCoins
+, -1 -- SxSellSupplies
+, 0 -- dbStatusID
+);
+-- Add building - Fruit Farm
+INSERT INTO FoE_SxBuildings (
+  SxID
+, SxName
+, SxAge
+, SxTech
+, SxPopRequired
+, SxSupplyYieldFiveMinutes_AA
+, SxSupplyYieldFifteenMinutes_BB
+, SxSupplyYieldOneHour_CC
+, SxSupplyYieldFourHours_DD
+, SxSupplyYieldEightHours_EE
+, SxSupplyYieldOneDay_FF
+, SxSupplyYieldTwoDays_GG
+, SxSWPixels
+, SxNWPixels
+, SxRoadWidth
+, SxBuildTime
+, SxBuildDiamonds
+, SxBuildCoins
+, SxBuildSupplies
+, SxSellCoins
+, SxSellSupplies
+, dbStatusID
+) VALUES (
+  150155 -- SxID
+, 'Fruit Farm' -- SxName
+, 'Bronze Age' -- SxAge
+, 'Cultivation' -- SxTech
+, 50 -- SxPopRequired
+, 22 -- SxSupplyYieldFiveMinutes_AA
+, 54 -- SxSupplyYieldFifteenMinutes_BB
+, 130 -- SxSupplyYieldOneHour_CC
+, 217 -- SxSupplyYieldFourHours_DD
+, 325 -- SxSupplyYieldEightHours_EE
+, 650 -- SxSupplyYieldOneDay_FF
+, -1 -- SxSupplyYieldTwoDays_GG
+, 5 -- SxSWPixels
+, 4 -- SxNWPixels
+, 1 -- SxRoadWidth
+, 2400 -- SxBuildTime
+, 0 -- SxBuildDiamonds
+, 1750 -- SxBuildCoins
+, 130 -- SxBuildSupplies
+, -1 -- SxSellCoins
+, -1 -- SxSellSupplies
+, 0 -- dbStatusID
+);
+-- Add building - Blacksmith
+INSERT INTO FoE_SxBuildings (
+  SxID
+, SxName
+, SxAge
+, SxTech
+, SxPopRequired
+, SxSupplyYieldFiveMinutes_AA
+, SxSupplyYieldFifteenMinutes_BB
+, SxSupplyYieldOneHour_CC
+, SxSupplyYieldFourHours_DD
+, SxSupplyYieldEightHours_EE
+, SxSupplyYieldOneDay_FF
+, SxSupplyYieldTwoDays_GG
+, SxSWPixels
+, SxNWPixels
+, SxRoadWidth
+, SxBuildTime
+, SxBuildDiamonds
+, SxBuildCoins
+, SxBuildSupplies
+, SxSellCoins
+, SxSellSupplies
+, dbStatusID
+) VALUES (
+  150160 -- SxID
+, 'Blacksmith' -- SxName
+, 'Bronze Age' -- SxAge
+, 'Smithery' -- SxTech
+, 12 -- SxPopRequired
+, -1 -- SxSupplyYieldFiveMinutes_AA
+, -1 -- SxSupplyYieldFifteenMinutes_BB
+, 32 -- SxSupplyYieldOneHour_CC
+, -1 -- SxSupplyYieldFourHours_DD
+, -1 -- SxSupplyYieldEightHours_EE
+, -1 -- SxSupplyYieldOneDay_FF
+, -1 -- SxSupplyYieldTwoDays_GG
+, 2 -- SxSWPixels
+, 2 -- SxNWPixels
+, 1 -- SxRoadWidth
+, 1200 -- SxBuildTime
+, 0 -- SxBuildDiamonds
+, 425 -- SxBuildCoins
+, 39 -- SxBuildSupplies
+, -1 -- SxSellCoins
+, -1 -- SxSellSupplies
+, -10010 -- dbStatusID
+);
